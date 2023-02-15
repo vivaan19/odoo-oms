350bd38450b4a6943292f5510699c01565b0cee0
vivaan19 <shiromanivivan@gmail.com>
2023-02-15 19:01:10 +0530

    added inventory custom module

--- Office_Management_Odoo/employee/models/__init__.py
+++ Office_Management_Odoo/employee/models/__init__.py
@@ -1,3 +1,4 @@
 from . import office_employee
 from . import employee_attendence
 from . import employee_join
+from . import inventory_replenishment
--- Office_Management_Odoo/employee/models/inventory_replenishment.py
+++ Office_Management_Odoo/employee/models/inventory_replenishment.py
@@ -2,4 +2,18 @@ from odoo import models, fields, api
 
 class InventoryReplenishment(models.Model):
     _inherit = "stock.warehouse.orderpoint"
+
+    quantity_diff = fields.Float(string="Difference",compute="_compute_quantity_diff")
+
+    def _compute_quantity_diff(self):
+        for rec in self:
+            rec.quantity_diff = rec.product_max_qty - rec.product_min_qty
+            
+    # @api.onchange('product_min_qty')
+    # def _onchange_product_id(self):
+    #     self.quantity_diff = self.product_max_qty - self.product_min_qty
+    
+    # @api.onchange('product_max_qty')
+    # def _onchange_product_id(self):
+    #     self.quantity_diff = self.product_max_qty - self.product_min_qty
     
