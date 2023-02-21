7baa8a85b3e4ad5b1c63b68fb0b92aa3c9551168
vivaan19 <shiromanivivan@gmail.com>
2023-02-21 18:19:10 +0530

    formatted docs - wizards, menu, inventory

--- Office_Management_Odoo/employee/wizards/cancel_emp_rec.py
+++ Office_Management_Odoo/employee/wizards/cancel_emp_rec.py
@@ -1,16 +1,17 @@
-from odoo import fields, models, api 
+from odoo import fields, models, api
+
 
 class CancelEmpRec(models.TransientModel):
     _name = 'cancel.emp.rec'
-    _description = 'Cancel Employee recruitment' 
+    _description = 'Cancel Employee recruitment'
     _rec_name = "emp_id"
 
     emp_id = fields.Many2one("employee.join", string="Employee Id")
-    
+
     emp_name = fields.Char("Name", related='emp_id.name')
-    
+
     emp_job_apply = fields.Selection(
-        string='Add department', help='This is for selection of departments in an office', 
+        string='Add department', help='This is for selection of departments in an office',
         related='emp_id.department_id.dept_name', readonly=True
     )
 
@@ -18,17 +19,14 @@ class CancelEmpRec(models.TransientModel):
         string="Experience Required in years", related='emp_id.dept_exp', readonly=True)
 
     # job type
-    dept_job_type = fields.Selection(string="Job type", related='emp_id.dept_job_type', readonly=True)
-
+    dept_job_type = fields.Selection(
+        string="Job type", related='emp_id.dept_job_type', readonly=True)
 
     emp_rem_reason = fields.Text("Application Removal Reason")
-    
+
     # @api.onchange('emp_id')
     # def _onchange_emp_id(self):
-    #     self.emp_name = self.emp_id.name  
-    
+    #     self.emp_name = self.emp_id.name
+
     def cancel_emp_application(self):
-        self.env['employee.join'].search([ ('can_id', '=', 'emp_id') ]).unlink()
-
-
-    
+        self.env['employee.join'].search([('can_id', '=', 'emp_id')]).unlink()
