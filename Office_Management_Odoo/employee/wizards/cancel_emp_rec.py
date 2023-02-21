88901cb69d1986f39d212a73c28ea4dbc8863dfd
vivaan19 <shiromanivivan@gmail.com>
2023-02-21 11:49:15 +0530

    wizard 3rd try failed

--- Office_Management_Odoo/employee/wizards/cancel_emp_rec.py
+++ Office_Management_Odoo/employee/wizards/cancel_emp_rec.py
@@ -5,13 +5,17 @@ class CancelEmpRec(models.TransientModel):
     _description = 'Cancel Employee recruitment' 
     _rec_name = "emp_id"
 
-    emp_id = fields.Many2one("candidate.applied", string="Employee Id")
-    emp_name = fields.Char("Name")
+    emp_id = fields.Many2one("employee.join", string="Employee Id")
+    
+    emp_name = fields.Char("Name", related='emp_id.name')
+    
+    emp_job_apply = fields.Many2one("", related='emp_id.department_id')
+
     emp_rem_reason = fields.Text("Application Removal Reason")
     
-    @api.onchange('emp_id')
-    def _onchange_emp_id(self):
-        self.emp_name = self.emp_id.can_applied.name
+    # @api.onchange('emp_id')
+    # def _onchange_emp_id(self):
+    #     self.emp_name = self.emp_id.name  
     
     def cancel_emp_application(self):
         self.env['employee.join'].search([ ('can_id', '=', 'emp_id') ]).unlink()
